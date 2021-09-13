dc75f7a81f36b8e042ccc935596e40451489cbbd
Siard van den Bosch <84980377+siardv@users.noreply.github.com>
2021-09-13 08:00:34 +0200

    Update xlsx_to_spss.R

--- xlsx_to_spss.R
+++ xlsx_to_spss.R
@@ -1,12 +1,7 @@
-
-install.packages("xlsx")
-library(xlsx)
-
-
-
-xlsx_to_syntax <- function(xlsx, id, answer, value_label, user_na, save_to){
+xlsx_to_syntax <- function(path, id, answer, value_label, user_na, save_to){
+  `%!in%` <- Negate(`%in%`)
   get_syntax <- function(df, row, id, answer, vl, na) {
-    set <- df[row, which(vl[[1]][match(!df[row, ], vl[[2]])] %in%  na)][-answer]
+    set <- df[row, which(vl[[1]][match(df[row, ], vl[[2]])] %!in%  na)][-answer]
     pid <- ifelse(any(letters %in% tolower(strsplit(set[[id]], split = "")[[1]])), sQuote(set[[id]], ""), set[id])
     if (length(set) == 1) {
       paste0("\n/*", row, ">.\nDO IF ", names(df)[id], " = ", pid, ".\nRECODE ", gsub(",", " TO", toString(range(names(df)[-c(id, answer)]))), " (SYSMIS = ", 999, ").\nEND IF.\n/*", row, "<\n\n")
@@ -19,51 +14,25 @@ xlsx_to_syntax <- function(xlsx, id, answer, value_label, user_na, save_to){
              ".\nDO IF ", names(set)[id], " = ", pid, ".\nRECODE X (SYSMIS = Y).\nEND IF.\nEND REPEAT.\n/*", row, "<\n\n")
     }
   }
-  sapply(xlsx, function(x) {
+  sapply(seq_along(path), function(i) {
     message("Creating syntax...")
-    df <- read.xlsx(x, sheetIndex = 1)
-    df <- as.data.frame(apply(df, 2, function(x) gsub("^$", "No other problems", gsub("IC|DK|Cannot.*$|NA", "DK/NA/Cannot be coded", ifelse(is.na(x), "NA", x)))))
-    names(df) <- c(names(df)[id], names(df)[answer], paste0(names(df)[2], letters[1:(ncol(df) - 2)]))
-    sps <- paste0(gsub("^.*/|[.].*$", "", x), ".sps")
-    sink(sps, append = FALSE)
-    cat(paste0("/* syntax for ", gsub("^.*/", "~/", x), " (n = ", nrow(df), ").\n\n"))
-    cat(paste0("GET  FILE = '", x,"'.\nDATASET NAME DEMES WINDOW = FRONT.\n\n"))
+    df <- read.xlsx(path[i], sheetIndex = 1)
+    df <- as.data.frame(apply(df, 2, function(i) gsub("^$", "No other problems", gsub("IC|DK|Cannot.*$|NA", "DK/NA/Cannot be coded", ifelse(is.na(i), "NA",i)))))
+    names(df) <- c(names(df)[id], names(df)[answer], paste0(names(df)[answer], letters[seq(1, ncol(df) - 2)]))
+    sps_name <- paste0(gsub("^.*/|[.].*$", "", path[i]), ".sps")
     probs <- names(df)[-c(id, answer)]
-    cat(paste0("DO REPEAT\nX = ", toString(probs), ".\nCOMPUTE  X = $SYSMIS.\nFORMATS X (F3).\nMISSING VALUES X (", toString(user_na), ").\nEND REPEAT.\n\n"))
-    cat("VALUE LABELS ", probs[1], " TO " , tail(probs, 1), "\n")
-    cat(paste0(gsub(", ", "\n", toString(paste0(vl[[1]], " '", vl[[2]], "'"))), ".\nEXECUTE.\n"))
-    cat(unlist(sapply(1:nrow(df), function(i) unlist(ifelse(i == nrow(df), 
-                                                            list(paste0(get_syntax(df, i, id, answer, value_label, user_na), 
-                                                                        paste0("\nDO REPEAT ID = ", names(df)[id], ".\nDO IF NOT (SYSMIS(", probs[1],")) AND (", 
-                                                                               names(df)[id]," = ID).\nRECODE ", probs[1], " TO ", tail(probs[1], 1), " (SYSMIS = ", 992, ").\nELSE IF (SYSMIS(", probs[1],")) AND (", 
-                                                                               names(df)[id]," = ID).\nRECODE ", probs[1], " TO ", tail(probs[1], 1), " (SYSMIS = ", 999, ").\nEND IF.\nEND REPEAT.\nEXECUTE.\n"))), 
-                                                            list(get_syntax(df, i, id, answer, value_label, user_na)))))))
-    sink()
-    file.copy(sps, to = save_to)
-    message(paste0("Done! Saved to ", path.expand("~"), save_to))
+    c(paste0("/* syntax for ", gsub("^.*/", "~/", path[i]), " (n = ", nrow(df), ").\n\n"),
+      paste0("GET  FILE = '", path[i],"'.\nDATASET NAME DEMES WINDOW = FRONT.\n\n"),
+      paste0("DO REPEAT\nX = ", toString(probs), ".\nCOMPUTE  X = $SYSMIS.\nFORMATS X (F3).\nMISSING VALUES X (", toString(user_na), ").\nEND REPEAT.\n\n"),
+      paste0("VALUE LABELS ", probs[1], " TO " , tail(probs, 1), "\n"),
+      paste0(gsub(", ", "\n", toString(paste0(value_label[[1]], " '", value_label[[2]], "'"))), ".\nEXECUTE.\n"),
+      unlist(sapply(1:nrow(df), function(r) unlist(ifelse(r == nrow(df), 
+                                                          list(paste0(get_syntax(df, r, id, answer, value_label, user_na), 
+                                                                      paste0("\nDO REPEAT ID = ", names(df)[id], ".\nDO IF NOT (SYSMIS(", probs[1],")) AND (", 
+                                                                             names(df)[id]," = ID).\nRECODE ", probs[1], " TO ", tail(probs[1], 1), " (SYSMIS = ", 992, ").\nELSE IF (SYSMIS(", probs[1],")) AND (", 
+                                                                             names(df)[id]," = ID).\nRECODE ", probs[1], " TO ", tail(probs[1], 1), " (SYSMIS = ", 999, ").\nEND IF.\nEND REPEAT.\nEXECUTE.\n"))), 
+                                                          list(get_syntax(df, r, id, answer, value_label, user_na)))))))
+    file.copy(sps_name, to = save_to)
+    message(paste0("Done! Saved ", sps_name, " to ", path.expand("~"), save_to))
   })
 }
-
-
-vl <- list(value = c(1:24, 991, 992, 999),
-           label = c('Economy / Financial situation', 'Social security', 'Politics', 'Crime', 'Defense', 'Healthcare', 'Education', 
-                     'Income / Prince levels / Taxes', 'Employment', 'Traffic / Mobility', 'Housing', 'Environment', 'Population', 
-                     'Minorities', 'Norms and values', 'Media', 'European integration', 'Inequality / Poverty', 'Intolerance / Discrimination', 
-                     'Foreign policy / International security', 'Regulation / Big government', 'Polarisation / Dividedness', 'Immigration', 'Corona', 
-                     'There are no problems', 'No other problems', 'DK/NA/Cannot be coded'))
-
-
-xlsx_to_syntax(xlsx = paste0("/Users/siard/Documents/RU/SKON/MIP/MIP/", c("mip_DEMES", "mip_LISS", "mip_DPES"), ".xlsx"), 
-              id = 1,
-              answer = 2,
-              value_label = vl,
-              user_na = c(991, 992, 999),
-              save_to = "/Users/siard/Desktop")
-
-## Arguments     
-# xlsx            Directory to Excel file(s) containing pre-categorized answers
-# id              Individual-level identifier
-# answer          Original answer to open-ended question
-# value_label     List containing categories/labels used in the Excel file with corresponding values
-# user_na         Which values are missing values?
-# save_to         Directory to save the .sps file
