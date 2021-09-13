6037199d6c931c4c0c46086cde82e858d7a982c9
Siard van den Bosch <84980377+siardv@users.noreply.github.com>
2021-09-13 08:05:35 +0200

    Update xlsx_to_spss.R

--- xlsx_to_spss.R
+++ xlsx_to_spss.R
@@ -1,4 +1,4 @@
-xlsx_to_syntax <- function(path, id, answer, value_label, user_na, save_to){
+xlsx_to_sps <- function(path, id, answer, value_labelss, user_na, save_to){
   `%!in%` <- Negate(`%in%`)
   get_syntax <- function(df, row, id, answer, vl, na) {
     set <- df[row, which(vl[[1]][match(df[row, ], vl[[2]])] %!in%  na)][-answer]
@@ -25,13 +25,13 @@ xlsx_to_syntax <- function(path, id, answer, value_label, user_na, save_to){
       paste0("GET  FILE = '", path[i],"'.\nDATASET NAME DEMES WINDOW = FRONT.\n\n"),
       paste0("DO REPEAT\nX = ", toString(probs), ".\nCOMPUTE  X = $SYSMIS.\nFORMATS X (F3).\nMISSING VALUES X (", toString(user_na), ").\nEND REPEAT.\n\n"),
       paste0("VALUE LABELS ", probs[1], " TO " , tail(probs, 1), "\n"),
-      paste0(gsub(", ", "\n", toString(paste0(value_label[[1]], " '", value_label[[2]], "'"))), ".\nEXECUTE.\n"),
+      paste0(gsub(", ", "\n", toString(paste0(value_labels[[1]], " '", value_labels[[2]], "'"))), ".\nEXECUTE.\n"),
       unlist(sapply(1:nrow(df), function(r) unlist(ifelse(r == nrow(df), 
-                                                          list(paste0(get_syntax(df, r, id, answer, value_label, user_na), 
+                                                          list(paste0(get_syntax(df, r, id, answer, value_labels, user_na), 
                                                                       paste0("\nDO REPEAT ID = ", names(df)[id], ".\nDO IF NOT (SYSMIS(", probs[1],")) AND (", 
                                                                              names(df)[id]," = ID).\nRECODE ", probs[1], " TO ", tail(probs[1], 1), " (SYSMIS = ", 992, ").\nELSE IF (SYSMIS(", probs[1],")) AND (", 
                                                                              names(df)[id]," = ID).\nRECODE ", probs[1], " TO ", tail(probs[1], 1), " (SYSMIS = ", 999, ").\nEND IF.\nEND REPEAT.\nEXECUTE.\n"))), 
-                                                          list(get_syntax(df, r, id, answer, value_label, user_na)))))))
+                                                          list(get_syntax(df, r, id, answer, value_labels, user_na)))))))
     file.copy(sps_name, to = save_to)
     message(paste0("Done! Saved ", sps_name, " to ", path.expand("~"), save_to))
   })
